0ae031522a6808f3be1c15e18f811bdd767693de
Lê Gia Phú <phugiale2912@gmail.com>
2024-09-13 14:25:17 +0700

    feat: add joke evaluation agent

--- demo.py
+++ demo.py
@@ -27,26 +27,26 @@ comedian_agent = Agent(
     llm=llm,
     verbose=True
 )
-# audience_agent = Agent(
-#     role="Performance Expert",
-#     goal=dedent("""\
-#         Analyze the performance, explain what is the meaning of the performance. Evaluate the performance, 
-#         give the performance a score on a scale of 1 to 10 and explain why the score was give based on
-#         what criteria."""),
-#     backstory=dedent("""\
-#             You are an audience, specialized in evaluating the performance of artists. You explain the
-#             meaning of the performance. You give scores to the performance based on certain criteria."""),
-#     allow_delegation=False,
-#     llm=llm,
-#     verbose=True,
-# )
+audience_agent = Agent(
+    role="Performance Expert",
+    goal=dedent("""\
+        Analyze the performance, explain what is the meaning of the performance. Evaluate the performance, 
+        give the performance a score on a scale of 1 to 10 and explain why the score was give based on
+        what criteria."""),
+    backstory=dedent("""\
+            You are an audience, specialized in evaluating the performance of artists. You explain the
+            meaning of the performance. You give scores to the performance based on certain criteria."""),
+    allow_delegation=False,
+    llm=llm,
+    verbose=True,
+)
 
 # Define task
 joke_generate_task = Task(
     description=dedent(f"""\
         Given the information about the user specifications, analyze the user input and extract the neccessary information,
         no yapping:
-            User inpit: {user_specs}.
+            User input: {user_specs}.
         
         Based on the information generate short but funny joke about the provided topic. No yapping.
         
@@ -56,25 +56,26 @@ joke_generate_task = Task(
         """),
     agent=comedian_agent,
 )
-# evaluate_task = Task(
-#     description=dedent("""\
-#         Based on the performance result, analyze the performance, explain what is the meaning of the performance.
-#         No yapping.
+evaluate_task = Task(
+    description=dedent("""\
+        Based on the performance result, analyze the performance, explain what is the meaning of the performance.
+        No yapping.
         
-#         Evaluate the performance, give the performance a score on a scale of 1 to 10 and explain why the score 
-#         was give based on what criteria. No yapping."""),
-#     expected_output=dedent("""\
-#         Two paragraphs seperated by spaces.
-#         The first paragraph is the original performance with the explanation.
-#         The second paragraph is the score given for the performance with criteria."""),
-#     agent=audience_agent,
-#     # context=[generate_task]
-# )
+        Evaluate the performance, give the performance a score on a scale of 1 to 10 and explain why the score 
+        was give based on what criteria. No yapping."""),
+    expected_output=dedent("""\
+        Three paragraphs seperated.
+        The first paragraph display the original performance.
+        The second paragraph is the explanation of the performance.
+        The second paragraph is the score given for the performance with criteria."""),
+    agent=audience_agent,
+    # context=[generate_task]
+)
 
 # Define comedian crew
 crew = Crew(
-    agents=[comedian_agent],
-    tasks=[joke_generate_task],
+    agents=[comedian_agent, audience_agent],
+    tasks=[joke_generate_task, evaluate_task],
     verbose=True,
     process=Process.sequential
 )
