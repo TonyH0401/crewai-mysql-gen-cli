a57604e8a3eb4512e758597ef0f6497cb98d1429
Lê Gia Phú <phugiale2912@gmail.com>
2024-09-13 10:08:59 +0700

    refactor+perf: change poem agent to mysql agent + prompt eng

--- configs/agents.py
+++ configs/agents.py
@@ -6,40 +6,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class PoemGenerateAgent:
+class MySQLGenerateAgent:
     def __init__(self):
         GROQ_API = os.getenv("GROQ_API", "")
         self.llm = ChatGroq(api_key=GROQ_API, model="llama3-8b-8192")
         # self.llm = ChatGroq(api_key=GROQ_API, model="llama3-70b-8192")
         # self.llm = ChatGroq(api_key=GROQ_API, model="gemma2-9b-it")
 
-    def poem_generate_agent(self):
+    def mysql_generate_agent(self):
         return Agent(
-            role="Poem Expert",
+            role="MySQL Expert",
             goal=dedent("""\
-                    Analyze keywords and request from the user,
-                    generate a poem based on the user specifications and requirements."""),
+                Analyze the user specifications and database schemas information, generate syntactically correct MySQL query 
+                based on the specifications and information while adhering to strict guidelines."""),
             backstory=dedent("""\
-                    You are famous Poet living in Acient Greek,
-                    you specialize in making beautiful poems (both long and short poems) with deep meaning."""),
+                As a MySQL Expert, you have extensive experiences with creating syntactically correct and efficient MySQL query,
+                you specialize in creating syntactically correct MySQL query and evaluating MySQL query while adheres to rules
+                set by the database schemas."""),
             allow_delegation=False,
             llm=self.llm,
             verbose=True,
         )
-
-    # def mysql_generate_agent(self):
-    #     return Agent(
-    #         role="MySQL Expert",
-    #         goal="Generate syntactically correct MySQL query based on user specifications while adhering to strict guidelines",
-    #         backstory="""
-    #             You are a MySQL expert with extensive experience in creating syntactically correct, precise and efficient MySQL query.
-    #             Your expertise and skills ensures that every query generated is syntactically correct and adheres to the rules set
-    #             by the database schemas.
-    #         """,
-    #         allow_delegation=False,
-    #         llm=self.llm,
-    #         verbose=True,
-    #     )
 
     # def mysql_extract_agent(llm):
     #     return Agent(
