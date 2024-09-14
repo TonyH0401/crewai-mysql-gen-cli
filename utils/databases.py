c28ea323a964706d08e75377aa33e99f7e8350a8
Lê Gia Phú <phugiale2912@gmail.com>
2024-09-14 14:05:30 +0700

    feat: convert schemas to json md

--- utils/databases.py
+++ utils/databases.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 from crewai import Agent, Task, Crew, Process
 from langchain_groq import ChatGroq
 import os
@@ -24,7 +25,47 @@ def read_mysql_db(filepath):
 def convert_schemas_to_json_markdown(raw_schemas):
     GROQ_API = os.getenv("GROQ_API", "")
     llm = ChatGroq(api_key=GROQ_API, model="llama3-8b-8192")
+    json_agent = Agent(
+        role="Information Extractor Expert",
+        goal=dedent("""\
+            Extract information from columns provided by the schemas. Convert the extracted information into JSON format."""),
+        backstory=dedent("""\
+            As an Information Extractor Expert, you have extensive experiences with extracting information from the 
+            provided schemas and category them in the correct JSON format."""),
+        allow_delegation=False,
+        llm=llm,
+        # verbose=True,
+    )
+    extract_task = Task(
+        description=dedent(f"""\
+            Given the information about the database schemas, analyze the database, focus on identifying information of 
+            the database schemas:    
+                - Schemas: {raw_schemas}
 
-    return None
+            Extract the information from database schemas. Strictly follow these goals and rules below. No yapping.
+            DO NOT make up information. DO NOT skip this step.
+                - DO:
+                    - The schema table name is the JSON object name.
+                    - Each column is a property in the JSON object.
+                    - The value of each property is the datatype of each column.
+                    - Include property for "PRIMARY KEY" and "CONSTRAINT".
+                    - Extract the column information from the schemas and return the extracted data in JSON format.
+                    - Return ONLY the JSON format.
+                - DO NOT:
+                    - Add "Here is the extracted data in JSON format:" or any similar lines. 
+
+            The final result is a JSON object containing the converted information of the database schemas."""),
+        expected_output=dedent("""\
+            A JSON object containing the converted information of the database schemas."""),
+        agent=json_agent
+    )
+    json_convert_crew = Crew(
+        agents=[json_agent],
+        tasks=[extract_task],
+        # verbose=True,
+        process=Process.sequential
+    )
+    output = json_convert_crew.kickoff()
+    return output
 
 # def
