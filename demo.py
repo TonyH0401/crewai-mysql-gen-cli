10607ca9b3ae0cd16b788c9a2b2e1baf34c906b9
Lê Gia Phú <phugiale2912@gmail.com>
2024-10-02 10:27:55 +0700

    test: complete Varargs testing

--- demo.py
+++ demo.py
@@ -2,7 +2,10 @@ def demo_func(propA, propB, *context):
     print(propA)
     print(propB)
     print(type(context))
+    print(type(list(context)))
+    print(list(context))
 
 # the "context" by the docs is a List()
 
+# demo_func("A", "B", 1)
 demo_func("A", "B", 1, 2, 3)
